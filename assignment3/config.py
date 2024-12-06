f081f71057e9fb62339383403b584b59acdc55f4
sahil1610patel <119137109+sahil1610patel@users.noreply.github.com>
2024-12-06 02:45:57 -0500

    Add files via upload

---
+++ assignment3/config.py
@@ -0,0 +1,24 @@
+# AWS Credentials
+AWS_ACCESS_KEY = 'Your_AWS_ACCESS_KEY'
+AWS_SECRET_KEY = 'Your_AWS_SECRET_KEY'
+AWS_REGION = 'Your_region'  # e.g., 'us-west-2'
+
+# Email Configuration
+SENDER_EMAIL = 'sahil1610patel@gmail.com'  # Replace with your sender email
+RECEIVER_EMAIL = 'Your_email'  # Replace with the recipient email
+EMAIL_PASSWORD = 'password'  # Replace with your email password (use app-specific password for Gmail if necessary)
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 587  # Standard SMTP port
+
+# Budget Threshold for AWS Cost Alert
+BUDGET_THRESHOLD = 10.00  # Set your spending threshold in USD
+
+# Forecast Days for Budget Prediction
+FORECAST_DAYS = 30  # The number of days to forecast for budget estimation
+
+# Cost Alert Threshold
+COST_THRESHOLD = 100.00  # The spending threshold to trigger an alert (USD)
+
+# EC2 Instance Configuration
+InstanceId = 'your_instance_id'
+
